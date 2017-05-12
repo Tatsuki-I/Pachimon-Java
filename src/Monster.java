33a49b86a007495d3adaee66ced11369fb4e4240
Tatsuki <tatsuki@konokaze.jp>
2017-05-12 14:20:40 +0900

    maxHpを追加

--- src/Monster.java
+++ src/Monster.java
@@ -1,19 +1,21 @@
 
 public class Monster {
-	
+
 	String name;
-	
+
 	int level;
-	
+
 	String species;
-	
+
 	int hp;
-	
+
+	int maxHp;
+
 	int atk;
-	
+
 	int def;
-	
+
 	int speed;
-	
+
 	char gender;
 }
