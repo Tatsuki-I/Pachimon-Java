6f981e23886da885fe6fc9150c23d7a25bb86da8
Tatsuki <tatsuki@konokaze.jp>
2017-05-13 00:14:17 +0900

    微修正(但し未だバグってます)

--- src/Skills.java
+++ src/Skills.java
@@ -3,8 +3,10 @@
  */
 public class Skills {
     Skill skills[];
+    final int NUM_OF_SKILL = 4;
 
     Skills(final int SKILL_ID0, final int SKILL_ID1, final int SKILL_ID2, final int SKILL_ID3) {
+        this.skills = new Skill[this.NUM_OF_SKILL];
         this.skills[0].setSkill(SKILL_ID0);
         this.skills[1].setSkill(SKILL_ID1);
         this.skills[2].setSkill(SKILL_ID2);
