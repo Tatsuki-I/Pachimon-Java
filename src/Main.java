71bdc805d6ffdba58b311c742daabc0411db7466
Tatsuki <tatsuki@konokaze.jp>
2017-05-13 00:53:43 +0900

    微調整

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		Finbashu test = new Finbashu('♂', "ぐんし", 3, new int[] {1, 0, 0, 0});
+		Finbashu test = new Finbashu('♂', "ぐんし", new java.util.Random().nextInt(9) + 1, new int[] {1, 0, 0, 0});
 
 		test.monster.showAllStatus();
 		test.monster.skills.showSkills();
