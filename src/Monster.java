ae10ebf307716b6156fb5fb0a79a63104584bfd0
bisce <iyi2adpke@i.softbank.jp>
2017-05-12 16:50:44 +0900

    新規実装

--- src/Monster.java
+++ src/Monster.java
@@ -4,25 +4,24 @@ public class Monster {
 	String name;
 
 	int level;
-
+	
+	final int maxLevel = 100;
+	
 	String species;
 
 	int currentHp;
 
 	int maxHp;
 
-	int atk;
-
-	int def;
-
-	int speed;
-
 	char gender;
 	
 	Type myType;
 	
-	Monster(final String NAME, final int TYPE_NUM){
+	final int syuzokuti;
+	
+	Monster(final String NAME, final int TYPE_NUM, final int SYUZOKUTI, final char GENDER){
 		this.name = NAME;
 		this.myType.setType(TYPE_NUM);
+		this.syuzokuti = SYUZOKUTI;
 	}
 }
