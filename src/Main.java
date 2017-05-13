a8300938785939e6b603c7f90dfba12587b76792
Tatsuki <tatsuki@konokaze.jp>
2017-05-13 21:11:27 +0900

    bug fix

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
-		Finbashu fin1 = new Finbashu('♂', "ぐんし", 3);
+		Finbashu fin1 = new Finbashu('♂', "ぐんし", 3, new int[]{1, 0, 0, 0});
 
-		Taiking  tai1 = new Taiking('♀',"近藤",5);
+		Taiking  tai1 = new Taiking('♀',"近藤",5, new int[]{1, 0, 0, 0});
 		
 		fin1.monster.showAllStatus();
 		
@@ -14,11 +13,9 @@ public class Main {
 		tai1.monster.showAllStatus();
 		
 		
-=======
 		Finbashu test = new Finbashu('♂', "ぐんし", new java.util.Random().nextInt(9) + 1, new int[] {1, 0, 0, 0});
 
 		test.monster.showAllStatus();
 		test.monster.skills.showSkills();
->>>>>>> 5d25da8448007d7907b576af18bc67e5b0b275b4
 	}
 }
--- src/Taiking.java
+++ src/Taiking.java
@@ -2,7 +2,7 @@
 public class Taiking {
     Monster monster;
 
-    Taiking(final char GENDER, String N_NAME, int C_LEVEL) {
-        this.monster = new Monster("タイキング", 1, 100, N_NAME, GENDER, C_LEVEL);
+    Taiking(final char GENDER, String N_NAME, int C_LEVEL, final int SKILL_ID[]) {
+        this.monster = new Monster("タイキング", 1, 100, N_NAME, GENDER, C_LEVEL, SKILL_ID);
     }
 }
