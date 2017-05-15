60f9fe5ac71a2164e3fd1aac65009d9efaad229d
Tatsuki <tatsuki@konokaze.jp>
2017-05-15 12:20:00 +0900

    bug fix

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@ public class Main {
 	public static void main(String[] args) {
 		Finbashu fin1 = new Finbashu('♂', "ぐんし", 30, new int[]{1, 0, 0, 0});
 		Taiking  tai1 = new Taiking('♀',"近藤",50, new int[]{1, 0, 2, 3});
-		
+
 		fin1.monster.showAllStatus();
 		tai1.monster.showAllStatus();
 
 		tai1.monster.showAllStatus();
 		new Battle().startBattle(fin1.monster, tai1.monster, true);
-		//tai1.monster.getDamage(fin1.monster, 0);
-		//tai1.monster.showAllStatus();
 	}
 }
