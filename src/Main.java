ae2f88548ea2a12f9535157d487b70edc38879db
Tatsuki <tatsuki@konokaze.jp>
2017-05-15 12:16:19 +0900

    remove unused instance

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,11 @@
 public class Main {
-
 	public static void main(String[] args) {
-
 		Finbashu fin1 = new Finbashu('♂', "ぐんし", 30, new int[]{1, 0, 0, 0});
-
 		Taiking  tai1 = new Taiking('♀',"近藤",50, new int[]{1, 0, 2, 3});
 		
 		fin1.monster.showAllStatus();
-		
-		System.out.println();
 		tai1.monster.showAllStatus();
-		
-		
-		Finbashu test = new Finbashu('♂', "ぐんし", new java.util.Random().nextInt(9) + 1, new int[] {1, 0, 0, 0});
 
-		test.monster.showAllStatus();
-		test.monster.skills.showSkills();
 		tai1.monster.showAllStatus();
 		new Battle().startBattle(fin1.monster, tai1.monster, true);
 		//tai1.monster.getDamage(fin1.monster, 0);
