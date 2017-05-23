90917820635013c94e4ce8553b537e1aa54b1f24
seno <iyi2adpke@i.softbank.jp>
2017-05-23 12:26:16 +0900

    調整

--- src/Map.java
+++ src/Map.java
@@ -5,11 +5,12 @@ public class Map {
 	static int y = 7;
 	final int HEIGHT = 10;
 	final int WIDTH = 10;
-
+	String kondoAlart = "ここだべ";
 	int[][] map = new int[this.HEIGHT][this.WIDTH];
-		
+	
+	String str = null;
 	void display(final int Y,final int X){
-		
+
 		map = new int[this.HEIGHT][this.WIDTH];
 		for(int i = 0;i < 4;i++){
 			for(int j = 0;j < 3;j++){
@@ -23,30 +24,41 @@ public class Map {
 		this.map[5][9] = 2;
 		if(this.map[Y][X] == 1 && new java.util.Random().nextInt(3) == 1){
 			System.out.println("野生の近藤が出現した");
+			this.kondoAlart = "野生の近藤が出現した";
+			
+		} else {
+			this.kondoAlart = "";
 		}
 		this.map[Y][X] = 5;
-		
+		this.str = "<html><font size=\"6\">";
 		for(int i = 0,j = 0;i < 10;j++){
 			switch(map[i][j]){
 				case 0:
+					str += "<font color=\"black\">道";
 					System.out.print("道");
 					break;
 				case 1:
+					str += "<font color=\"green\">Ｗ";
 					System.out.print("Ｗ");
 					break;
 				case 2:
+					str += "<font color=\"blue\">川";
 					System.out.print("川");
 					break;
 				case 5:
+					str += "<font color=\"red\">人";
 					System.out.print("人");
 					break;
 			}
 			if(j == 9){
 				j = -1;
 				i++;
+				str += "<br>";
 				System.out.println();
 			}
+			
 		}
+		str += "</html></font>";
 	}
 	void upMove(){
 		if (this.y > 0 && this.map[this.y - 1][this.x] != 2){
