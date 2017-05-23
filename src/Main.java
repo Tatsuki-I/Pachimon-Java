fbeedff597a4b4dbea39a08a47b799d00726c93c
bisce <iyi2adpke@i.softbank.jp>
2017-05-23 14:53:08 +0900

    main変更

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
 	public static void main(String[] args) {
-		Finbashu fin1 = new Finbashu('♂', "ぐんし", 30, new int[]{1, 0, 0, 0});
+		
+		Gui ts1 = new Gui("パチモン", new Map());
+		
+		/*Finbashu fin1 = new Finbashu('♂', "ぐんし", 30, new int[]{1, 0, 0, 0});
 		Taiking  tai1 = new Taiking('♀',"近藤",50, new int[]{1, 0, 2, 3});
 
 		fin1.monster.showAllStatus();
 		tai1.monster.showAllStatus();
 
 		tai1.monster.showAllStatus();
-		new Battle().startBattle(fin1.monster, tai1.monster, true);
+		new Battle().startBattle(fin1.monster, tai1.monster, true);*/
 	}
 }
