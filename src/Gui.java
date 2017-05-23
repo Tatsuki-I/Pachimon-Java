c24f0554976c6b70ec91a40ed4a8deb8f4238686
seno <iyi2adpke@i.softbank.jp>
2017-05-23 12:25:28 +0900

    調整

--- src/Gui.java
+++ src/Gui.java
@@ -1,64 +1,111 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Gui implements ActionListener {
 
 	Map map;
+	JFrame frame;
+
+	JButton UP;
+	JButton DOWN;
+	JButton LEFT;
+	JButton RIGHT;
+
+	JLabel display;
+	JLabel appearKondo;
+
+	JPanel board;
+	JPanel pal;
+	JPanel panel;
+
 	Gui(String title, Map mapClass){
 
 
-		JFrame frame = new JFrame(title);
+		this.frame = new JFrame(title);
 
-		frame.setBounds(100,100,800,600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setSize(600,420);
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JButton UP = new JButton("↑");
-		UP.addActionListener(this);
-		UP.setActionCommand("UP");
+		this.UP = new JButton("↑");
+		this.DOWN = new JButton("↓");
+		this.LEFT = new JButton("←");
+		this.RIGHT = new JButton("→");
 
-		JButton DOWN = new JButton("↓");
-		DOWN.addActionListener(this);
-		DOWN.setActionCommand("DOWN");
-		
-		JButton LEFT = new JButton("←");
-		LEFT.addActionListener(this);
-		LEFT.setActionCommand("LEFT");
-		
-		JButton RIGHT = new JButton("→");
-		RIGHT.addActionListener(this);
-		RIGHT.setActionCommand("RIGHT");
+		this.UP.setActionCommand("UP");
+		this.DOWN.setActionCommand("DOWN");
+		this.LEFT.setActionCommand("LEFT");
+		this.RIGHT.setActionCommand("RIGHT");
 
-		JPanel OPRATION = new JPanel();
+		this.appearKondo = new JLabel();
 
-		OPRATION.add(UP);
-		OPRATION.add(DOWN);
-		OPRATION.add(LEFT);
-		OPRATION.add(RIGHT);
-		
+		this.board = new JPanel();
+		this.display = new JLabel();
 
-		frame.getContentPane().add(OPRATION);
-		frame.setVisible(true);
+		this.board.add(display,BorderLayout.NORTH);
+		this.board.add(appearKondo,BorderLayout.SOUTH);
+
+
+
+		this.UP.addActionListener(this);
+		this.DOWN.addActionListener(this);
+		this.LEFT.addActionListener(this);
+		this.RIGHT.addActionListener(this);
+
+		this.panel = new JPanel();
+		this.panel.setLayout(new GridLayout(3,3));;
+
+		this.panel.add(new JPanel());
+		this.panel.add(UP);
+		this.panel.add(new JPanel());
+		this.panel.add(LEFT);
+		this.panel.add(new JPanel());
+		this.panel.add(RIGHT);
+		this.panel.add(new JPanel());
+		this.panel.add(DOWN);
+		this.panel.add(new JPanel());
+
+		this.pal = new JPanel();
+		this.pal.setLayout(new GridLayout(0,2));;
+
+		this.pal.add(this.board);
+		this.pal.add(this.panel);
+
+		this.frame.getContentPane().add(this.pal);
+
+		this.frame.setVisible(true);
 		this.map = mapClass;
-		
+
 		this.map.display(this.map.y,this.map.x);
 		System.out.println();
+	}
+	void guiDisplay(){
+		this.display.setText(this.map.str);
+		this.appearKondo.setText(this.map.kondoAlart);
+		this.display.repaint();
 	}
 	public void actionPerformed(ActionEvent e) {
 		String cmd = e.getActionCommand();
 
 		if(cmd.equals("UP")){
 			this.map.upMove();
+			this.guiDisplay();
 		}else if(cmd.equals("DOWN")){
 			this.map.downMove();
+			this.guiDisplay();
 		}else if(cmd.equals("LEFT")){
 			this.map.leftMove();
+			this.guiDisplay();
 		}else if(cmd.equals("RIGHT")){
 			this.map.rightMove();
+			this.guiDisplay();
 		}
 	}
-
 }
+
